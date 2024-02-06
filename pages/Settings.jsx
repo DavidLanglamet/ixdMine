ad59c0bbb563fd2d7e6cca000984c64d4a355f97
David Langlamet <david.langlamet@gmail.com>
2024-02-06 09:05:11 +0100

    made settingsback icon a little bigger

--- pages/Settings.jsx
+++ pages/Settings.jsx
@@ -7,7 +7,7 @@ const Settings = () => {
     <>
         <div style={{ position: 'absolute', bottom: '20px', right: '20px' }}>
           <Link to="/">
-            <button style={{ background: 'transparent', width: '100px' }}>
+            <button style={{ background: 'transparent', width: '110px' }}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
               </svg>
